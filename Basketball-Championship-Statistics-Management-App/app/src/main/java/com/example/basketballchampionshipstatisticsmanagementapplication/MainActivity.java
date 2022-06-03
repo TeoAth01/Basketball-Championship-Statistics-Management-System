67b4c21f2762ef2dfb6ec6aa94f8a2edd0b95e76
TeoAth01 <82370290+TeoAth01@users.noreply.github.com>
2022-06-03 18:48:09 +0300

    Finished R8

--- Basketball-Championship-Statistics-Management-App/app/src/main/java/com/example/basketballchampionshipstatisticsmanagementapplication/MainActivity.java
+++ Basketball-Championship-Statistics-Management-App/app/src/main/java/com/example/basketballchampionshipstatisticsmanagementapplication/MainActivity.java
@@ -3,19 +3,31 @@ package com.example.basketballchampionshipstatisticsmanagementapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonSend = findViewById(R.id.btnGoTestAct);
 
+        Button buttonSend = findViewById(R.id.btnGoTestAct);
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btnLoad = findViewById(R.id.buttonLoad);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
+
