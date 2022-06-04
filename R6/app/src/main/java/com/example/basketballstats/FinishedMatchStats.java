f8f5815b7b57ddb23e045c10a324f3d2cd655c21
GrandpaOfAll <79862308+GrandpaOfAll@users.noreply.github.com>
2022-06-04 19:55:04 +0300

    R6 Completed

--- R6/app/src/main/java/com/example/basketballstats/FinishedMatchStats.java
+++ R6/app/src/main/java/com/example/basketballstats/FinishedMatchStats.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -24,6 +26,8 @@ import java.util.Map;
 
 
 public class FinishedMatchStats extends AppCompatActivity {
+    private boolean isLeftListEnabled = true;
+    private boolean isRightListEnabled = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,24 +36,6 @@ public class FinishedMatchStats extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
-        ArrayList<String> a = new ArrayList<String>();
-        a.add("FTA");
-        a.add("FTM");
-        a.add("2PA");
-        a.add("2PM");
-        a.add("3PA");
-        a.add("3PM");
-        a.add("BLOCKS");
-        a.add("TURNOVERS");
-        a.add("FOULS");
-        a.add("STEALS");
-        a.add("REBOUNDS");
-        a.add("ASSISTS");
-        ListView list = findViewById(R.id.list);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, a);
-        list.setAdapter(arrayAdapter);
-
         new InfoAsyncTask().execute();
     }
 
@@ -69,7 +55,7 @@ public class FinishedMatchStats extends AppCompatActivity {
 
 
             try (Connection connection = DriverManager.getConnection("jdbc:mariadb://192.168.2.8/omada 18","username","password")) {
-
+                //Getting Home Teams Stats From Database
                 String sql = "SELECT HFTA,HFTM,H2PA,H2PM,H3PA,H3PM,HBLOCK,HTURNOVER,HFOUL,HSTEAL,HREBOUND,HASSIST FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
                 PreparedStatement statement = connection.prepareStatement(sql);
                 ResultSet resultSet = statement.executeQuery();
@@ -81,6 +67,7 @@ public class FinishedMatchStats extends AppCompatActivity {
                     info.add(home);
                 }
 
+                //Getting Home Teams Stats From Databse
                 String sql2 = "SELECT AFTA,AFTM,A2PA,A2PM,A3PA,A3PM,ABLOCK,ATURNOVER,AFOUL,ASTEAL,AREBOUND,AASSIST FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
                 PreparedStatement statement2 = connection.prepareStatement(sql2);
                 ResultSet resultSet2 = statement2.executeQuery();
@@ -92,6 +79,7 @@ public class FinishedMatchStats extends AppCompatActivity {
                     info.add(away);
                 }
 
+                //Getting Points and Names for both home and away teams from database
                 String sql3 = "SELECT HOME,HPOINTS,AWAY,APOINTS FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
                 PreparedStatement statement3 = connection.prepareStatement(sql3);
                 ResultSet resultSet3 = statement3.executeQuery();
@@ -103,6 +91,7 @@ public class FinishedMatchStats extends AppCompatActivity {
                     info.add(namesAndPoints);
                 }
 
+                //Getting both teams logos from database
                 if(!info.isEmpty()){
                     String sql4 = "SELECT Logo FROM teams WHERE name = '"+str1+"'";
                     PreparedStatement statement4 = connection.prepareStatement(sql4);
@@ -143,15 +132,36 @@ public class FinishedMatchStats extends AppCompatActivity {
             ImageView awayImage = (ImageView) findViewById(R.id.awayTeam_Logo);
             TextView homePoints = (TextView) findViewById(R.id.homePoints);
             TextView awayPoints = (TextView) findViewById(R.id.awayPoints);
+            ListView lv = (ListView) findViewById(R.id.homeList);
+            ListView lv1 = (ListView) findViewById(R.id.awayList);
+            ListView lv2 = findViewById(R.id.list);
+
 
             if (!result.isEmpty()) {
-                ListView lv = (ListView) findViewById(R.id.homeList);
+                //Filling ListViews
                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, result.get(0));
                 lv.setAdapter(arrayAdapter);
 
-                ListView lv1 = (ListView) findViewById(R.id.awayList);
+
                 ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, result.get(1));
                 lv1.setAdapter(arrayAdapter1);
+
+                ArrayList<String> a = new ArrayList<String>();
+                a.add("FTA");
+                a.add("FTM");
+                a.add("2PA");
+                a.add("2PM");
+                a.add("3PA");
+                a.add("3PM");
+                a.add("BLOCKS");
+                a.add("TURNOVERS");
+                a.add("FOULS");
+                a.add("STEALS");
+                a.add("REBOUNDS");
+                a.add("ASSISTS");
+
+                ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, a);
+                lv2.setAdapter(arrayAdapter2);
 
 
                 homeName.setText(result.get(2).get(0));
@@ -161,6 +171,80 @@ public class FinishedMatchStats extends AppCompatActivity {
                 Picasso.with(getApplicationContext()).load(result.get(3).get(0)).into(homeImage);
                 Picasso.with(getApplicationContext()).load(result.get(3).get(1)).into(awayImage);
             }
+
+            //Managing Scrolling On ListViews
+            lv.setOverScrollMode(ListView.OVER_SCROLL_NEVER);
+            lv1.setOverScrollMode(ListView.OVER_SCROLL_NEVER);
+            lv2.setOverScrollMode(ListView.OVER_SCROLL_NEVER);
+
+            lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+                @Override
+                public void onScrollStateChanged(AbsListView view, int scrollState) {
+                    // onScroll will be called and there will be an infinite loop.
+                    // That's why i set a boolean value
+                    if (scrollState == SCROLL_STATE_TOUCH_SCROLL) {
+                        isRightListEnabled = false;
+                    } else if (scrollState == SCROLL_STATE_IDLE) {
+                        isRightListEnabled = true;
+                    }
+                }
+
+                @Override
+                public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                     int totalItemCount) {
+                    View c = view.getChildAt(0);
+                    if (c != null && isLeftListEnabled) {
+                        lv1.setSelectionFromTop(firstVisibleItem, c.getTop());
+                        lv2.setSelectionFromTop(firstVisibleItem,c.getTop());
+                    }
+                }
+            });
+
+            lv1.setOnScrollListener(new AbsListView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(AbsListView view, int scrollState) {
+                    if (scrollState == SCROLL_STATE_TOUCH_SCROLL) {
+                        isLeftListEnabled = false;
+                    } else if (scrollState == SCROLL_STATE_IDLE) {
+                        isLeftListEnabled = true;
+                    }
+                }
+
+                @Override
+                public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                     int totalItemCount) {
+                    View c = view.getChildAt(0);
+                    if (c != null && isRightListEnabled) {
+                        lv.setSelectionFromTop(firstVisibleItem, c.getTop());
+                        lv2.setSelectionFromTop(firstVisibleItem,c.getTop());
+                    }
+                }
+            });
+
+            lv2.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+                @Override
+                public void onScrollStateChanged(AbsListView view, int scrollState) {
+                    // onScroll will be called and there will be an infinite loop.
+                    // That's why i set a boolean value
+                    if (scrollState == SCROLL_STATE_TOUCH_SCROLL) {
+                        isRightListEnabled = false;
+                    } else if (scrollState == SCROLL_STATE_IDLE) {
+                        isRightListEnabled = true;
+                    }
+                }
+
+                @Override
+                public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                     int totalItemCount) {
+                    View c = view.getChildAt(0);
+                    if (c != null && isLeftListEnabled) {
+                        lv.setSelectionFromTop(firstVisibleItem, c.getTop());
+                        lv1.setSelectionFromTop(firstVisibleItem, c.getTop());
+                    }
+                }
+            });
         }
     }
 }
