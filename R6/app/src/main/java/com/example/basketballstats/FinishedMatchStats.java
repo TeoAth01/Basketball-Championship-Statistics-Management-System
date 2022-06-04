5a9491d5c53daeb89a4c681bb6292d63a9753ef4
GrandpaOfAll <79862308+GrandpaOfAll@users.noreply.github.com>
2022-06-04 11:29:13 +0300

    Finished R6

--- R6/app/src/main/java/com/example/basketballstats/FinishedMatchStats.java
+++ R6/app/src/main/java/com/example/basketballstats/FinishedMatchStats.java
@@ -9,7 +9,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import java.sql.Connection;
@@ -31,68 +33,23 @@ public class FinishedMatchStats extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-//        TextView homeName = (TextView) findViewById(R.id.homeTeam_Name);
-//        TextView awayName = (TextView) findViewById(R.id.awayTeam_Name);
-//        ImageView homeImage = (ImageView) findViewById(R.id.homeTeam_Logo);
-//        ImageView awayImage = (ImageView) findViewById(R.id.awayTeam_Logo);
-//        TextView homePoints = (TextView) findViewById(R.id.homePoints);
-//        TextView awayPoints = (TextView) findViewById(R.id.awayPoints);
-//        TextView homeAssists = (TextView) findViewById(R.id.homeAssists);
-//        TextView awayAssists = (TextView) findViewById(R.id.awayAssists);
-//        TextView homeRebounds = (TextView) findViewById(R.id.homeRebounds);
-//        TextView awayRebounds = (TextView) findViewById(R.id.awayRebounds);
-//        TextView homeBlocks = (TextView) findViewById(R.id.homeBlocks);
-//        TextView awayBlocks = (TextView) findViewById(R.id.awayBlocks);
-//        TextView homeSteals = (TextView) findViewById(R.id.homeSteals);
-//        TextView awaySteals = (TextView) findViewById(R.id.awaySteals);
-//        TextView homeTurnovers = (TextView) findViewById(R.id.homeTurnovers);
-//        TextView awayTurnovers = (TextView) findViewById(R.id.awayTurnovers);
-//        TextView homeFouls = (TextView) findViewById(R.id.homeFouls);
-//        TextView awayFouls = (TextView) findViewById(R.id.awayFouls);
-//        TextView homeFT = (TextView) findViewById(R.id.homeFT);
-//        TextView awayFT = (TextView) findViewById(R.id.awayFT);
-//        TextView home2pt = (TextView) findViewById(R.id.home2PTS);
-//        TextView away2pt = (TextView) findViewById(R.id.away2PTS);
-//        TextView home3pt = (TextView) findViewById(R.id.home3PTS);
-//        TextView away3pt = (TextView) findViewById(R.id.away3PTS);
-//
-//
-//        awayName.setText(str2);
-//        homeName.setText(str1);
-//        Picasso.with(this).load("https://www.larisabasket.gr/images/newlogo1.png").into(homeImage);
-//        Picasso.with(this).load("https://www.gazzetta.gr/sites/default/files/styles/default/public/2021-01/aris-bc-min.png?itok=N7nFnxEb").into(awayImage);
-//        homePoints.setText("300");
-//        awayPoints.setText("0");
-//        homeAssists.setText("500");
-//        awayAssists.setText("0");
-//        homeRebounds.setText("500");
-//        awayRebounds.setText("0");
-//        homeBlocks.setText("500");
-//        awayBlocks.setText("0");
-//        homeSteals.setText("500");
-//        awaySteals.setText("0");
-//        homeTurnovers.setText("500");
-//        awayTurnovers.setText("0");
-//        homeFouls.setText("500");
-//        awayFouls.setText("0");
-//        homeFT.setText("500/500");
-//        awayFT.setText("0/500");
-//        home2pt.setText("500/500");
-//        away2pt.setText("0/500");
-//        home3pt.setText("500/500");
-//        away3pt.setText("0/500");
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("FTA");
+        a.add("FTM");
+        a.add("2PA");
+        a.add("2PM");
+        a.add("3PA");
+        a.add("3PM");
+        a.add("BLOCKS");
+        a.add("TURNOVERS");
+        a.add("FOULS");
+        a.add("STEALS");
+        a.add("REBOUNDS");
+        a.add("ASSISTS");
+        ListView list = findViewById(R.id.list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, a);
+        list.setAdapter(arrayAdapter);
 
-
-//        Button b1 = (Button) findViewById(R.id.button);
-//        b1.setOnClickListener(new View.OnClickListener() {
-//            int i = 0;
-//            @Override
-//            public void onClick(View view) {
-//                 TextView homePoint = (TextView) findViewById(R.id.homePoints);
-//                 i++;
-//                 homePoint.setText(String.valueOf(i));
-//            }
-//        });
         new InfoAsyncTask().execute();
     }
 
@@ -103,153 +60,101 @@ public class FinishedMatchStats extends AppCompatActivity {
             Intent intent = getIntent();
             String str1 = intent.getStringExtra("String 1");
             String str2 = intent.getStringExtra("String 2");
+
             ArrayList<ArrayList<String>> info = new ArrayList<ArrayList<String>>();
+            ArrayList<String> namesAndPoints = new ArrayList<String>();
+            ArrayList<String> home = new ArrayList<String>();
+            ArrayList<String> away = new ArrayList<String>();
+            ArrayList<String> logos = new ArrayList<String>();
+
 
             try (Connection connection = DriverManager.getConnection("jdbc:mariadb://192.168.2.8/omada 18","username","password")) {
-                info.add(new ArrayList<String>());
-                String sql = "SELECT * FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
+
+                String sql = "SELECT HFTA,HFTM,H2PA,H2PM,H3PA,H3PM,HBLOCK,HTURNOVER,HFOUL,HSTEAL,HREBOUND,HASSIST FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
                 PreparedStatement statement = connection.prepareStatement(sql);
                 ResultSet resultSet = statement.executeQuery();
                 int databaseColumnCount = resultSet.getMetaData().getColumnCount();
                 if (resultSet.next()) {
                     for (int j = 0; j < databaseColumnCount; j++) {
-                        info.get(0).add(resultSet.getString(j+1));
+                        home.add(resultSet.getString(j+1));
                     }
-//                    info.put("HOME", resultSet.getString("HOME"));
-//                    info.put("AWAY", resultSet.getString("AWAY"));
-//                    info.put("HOME FTA", resultSet.getString("HOME FTA"));
-//                    info.put("AWAY FTA", resultSet.getString("AWAY FTA"));
-//                    info.put("HOME FTM", resultSet.getString("HOME FTM"));
-//                    info.put("AWAY FTM", resultSet.getString("AWAY FTM"));
-//                    info.put("HOME 2PA", resultSet.getString("HOME 2PA"));
-//                    info.put("AWAY 2PA", resultSet.getString("AWAY 2PA"));
-//                    info.put("HOME 2PM", resultSet.getString("HOME 2PM"));
-//                    info.put("AWAY 2PM", resultSet.getString("AWAY 2PM"));
-//                    info.put("HOME 3PA", resultSet.getString("HOME 3PA"));
-//                    info.put("AWAY 3PA", resultSet.getString("AWAY 3PA"));
-//                    info.put("HOME 3PM", resultSet.getString("HOME 3PM"));
-//                    info.put("AWAY 3PM", resultSet.getString("AWAY 3PM"));
-//                    info.put("HOME ASSIST", resultSet.getString("HOME ASSIST"));
-//                    info.put("AWAY ASSIST", resultSet.getString("AWAY ASSIST"));
-//                    info.put("HOME REBOUND", resultSet.getString("HOME REBOUND"));
-//                    info.put("AWAY REBOUND", resultSet.getString("AWAY REBOUND"));
-//                    info.put("HOME BLOCK", resultSet.getString("HOME BLOCK"));
-//                    info.put("AWAY BLOCK", resultSet.getString("AWAY BLOCK"));
-//                    info.put("HOME STEAL", resultSet.getString("HOME STEAL"));
-//                    info.put("AWAY STEAL", resultSet.getString("AWAY STEAL"));
-//                    info.put("HOME TURNOVER", resultSet.getString("HOME TURNOVER"));
-//                    info.put("AWAY TURNOVER", resultSet.getString("AWAY TURNOVER"));
-//                    info.put("HOME FOUL", resultSet.getString("HOME FOUL"));
-//                    info.put("AWAY FOUL", resultSet.getString("AWAY FOUL"));
-//                    info.put("HOME POINTS", resultSet.getString("HOME POINTS"));
-//                    info.put("AWAY POINTS", resultSet.getString("AWAY POINTS"));
-
+                    info.add(home);
                 }
 
-                String sql2 = "SELECT Logo FROM teams WHERE name = '"+str1+"'";
+                String sql2 = "SELECT AFTA,AFTM,A2PA,A2PM,A3PA,A3PM,ABLOCK,ATURNOVER,AFOUL,ASTEAL,AREBOUND,AASSIST FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
                 PreparedStatement statement2 = connection.prepareStatement(sql2);
                 ResultSet resultSet2 = statement2.executeQuery();
-
+                int databaseColumnCount1 = resultSet2.getMetaData().getColumnCount();
                 if (resultSet2.next()) {
-                    info.get(0).add(resultSet2.getString(1));
+                    for (int j = 0; j < databaseColumnCount1; j++) {
+                        away.add(resultSet2.getString(j + 1));
+                    }
+                    info.add(away);
                 }
 
-                String sql3 = "SELECT Logo FROM teams WHERE name = '"+str2+"'";
+                String sql3 = "SELECT HOME,HPOINTS,AWAY,APOINTS FROM matches WHERE HOME='"+str1+"' AND AWAY='"+str2+"'";
                 PreparedStatement statement3 = connection.prepareStatement(sql3);
                 ResultSet resultSet3 = statement3.executeQuery();
-
+                int databaseColumnCount2 = resultSet3.getMetaData().getColumnCount();
                 if (resultSet3.next()) {
-                    info.get(0).add(resultSet3.getString(1));
+                    for (int j = 0; j < databaseColumnCount2; j++) {
+                        namesAndPoints.add(resultSet3.getString(j + 1));
+                    }
+                    info.add(namesAndPoints);
+                }
+
+                String sql4 = "SELECT Logo FROM teams WHERE name = '"+str1+"'";
+                PreparedStatement statement4 = connection.prepareStatement(sql4);
+                ResultSet resultSet4 = statement4.executeQuery();
+                int databaseColumnCount4 = resultSet4.getMetaData().getColumnCount();
+                if (resultSet4.next()) {
+                    for (int j = 0; j < databaseColumnCount4; j++) {
+                        logos.add(resultSet4.getString(j + 1));
+                    }
+                }
+
+                String sql5 = "SELECT Logo FROM teams WHERE name = '"+str2+"'";
+                PreparedStatement statement5 = connection.prepareStatement(sql5);
+                ResultSet resultSet5 = statement5.executeQuery();
+                int databaseColumnCount5 = resultSet5.getMetaData().getColumnCount();
+                if (resultSet5.next()) {
+                    for (int j = 0; j < databaseColumnCount5; j++) {
+                        logos.add(resultSet5.getString(j + 1));
+                    }
+                    info.add(logos);
                 }
 
             } catch (Exception e) {
                 Log.e("InfoAsyncTask", "Error reading school information", e);
             }
-
             return info;
         }
 
         @Override
         protected void onPostExecute(ArrayList<ArrayList<String>> result) {
+            TextView homeName = (TextView) findViewById(R.id.homeTeam_Name);
+            TextView awayName = (TextView) findViewById(R.id.awayTeam_Name);
+            ImageView homeImage = (ImageView) findViewById(R.id.homeTeam_Logo);
+            ImageView awayImage = (ImageView) findViewById(R.id.awayTeam_Logo);
+            TextView homePoints = (TextView) findViewById(R.id.homePoints);
+            TextView awayPoints = (TextView) findViewById(R.id.awayPoints);
+
             if (!result.isEmpty()) {
-                int cols = result.get(0).size();
-                TextView homeName = (TextView) findViewById(R.id.homeTeam_Name);
-                TextView awayName = (TextView) findViewById(R.id.awayTeam_Name);
-                ImageView homeImage = (ImageView) findViewById(R.id.homeTeam_Logo);
-                ImageView awayImage = (ImageView) findViewById(R.id.awayTeam_Logo);
-                TextView homePoints = (TextView) findViewById(R.id.homePoints);
-                TextView awayPoints = (TextView) findViewById(R.id.awayPoints);
-                TextView homeAssists = (TextView) findViewById(R.id.homeAssists);
-                TextView awayAssists = (TextView) findViewById(R.id.awayAssists);
-                TextView homeRebounds = (TextView) findViewById(R.id.homeRebounds);
-                TextView awayRebounds = (TextView) findViewById(R.id.awayRebounds);
-                TextView homeBlocks = (TextView) findViewById(R.id.homeBlocks);
-                TextView awayBlocks = (TextView) findViewById(R.id.awayBlocks);
-                TextView homeSteals = (TextView) findViewById(R.id.homeSteals);
-                TextView awaySteals = (TextView) findViewById(R.id.awaySteals);
-                TextView homeTurnovers = (TextView) findViewById(R.id.homeTurnovers);
-                TextView awayTurnovers = (TextView) findViewById(R.id.awayTurnovers);
-                TextView homeFouls = (TextView) findViewById(R.id.homeFouls);
-                TextView awayFouls = (TextView) findViewById(R.id.awayFouls);
-                TextView homeFT = (TextView) findViewById(R.id.homeFT);
-                TextView awayFT = (TextView) findViewById(R.id.awayFT);
-                TextView home2pt = (TextView) findViewById(R.id.home2PTS);
-                TextView away2pt = (TextView) findViewById(R.id.away2PTS);
-                TextView home3pt = (TextView) findViewById(R.id.home3PTS);
-                TextView away3pt = (TextView) findViewById(R.id.away3PTS);
+                ListView lv = (ListView) findViewById(R.id.homeList);
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, result.get(0));
+                lv.setAdapter(arrayAdapter);
+
+                ListView lv1 = (ListView) findViewById(R.id.awayList);
+                ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, result.get(1));
+                lv1.setAdapter(arrayAdapter1);
 
 
-                homeName.setText(result.get(0).get(1));
-                awayName.setText(result.get(0).get(2));
-                homeFT.setText(result.get(0).get(5)+"/"+result.get(0).get(3));
-                awayFT.setText(result.get(0).get(6)+"/"+result.get(0).get(4));
-                home2pt.setText(result.get(0).get(9)+"/"+result.get(0).get(7));
-                away2pt.setText(result.get(0).get(10)+"/"+result.get(0).get(8));
-                home3pt.setText(result.get(0).get(13)+"/"+result.get(0).get(11));
-                away3pt.setText(result.get(0).get(14)+"/"+result.get(0).get(12));
-                homeAssists.setText(result.get(0).get(15));
-                awayAssists.setText(result.get(0).get(16));
-                homeRebounds.setText(result.get(0).get(17));
-                awayRebounds.setText(result.get(0).get(18));
-                homeBlocks.setText(result.get(0).get(19));
-                awayBlocks.setText(result.get(0).get(20));
-                homeSteals.setText(result.get(0).get(21));
-                awaySteals.setText(result.get(0).get(22));
-                homeTurnovers.setText(result.get(0).get(23));
-                awayTurnovers.setText(result.get(0).get(24));
-                homeFouls.setText(result.get(0).get(25));
-                awayFouls.setText(result.get(0).get(26));
-                homePoints.setText(result.get(0).get(27));
-                awayPoints.setText(result.get(0).get(28));
-                Picasso.with(getApplicationContext()).load(result.get(0).get(30)).into(homeImage);
-                Picasso.with(getApplicationContext()).load(result.get(0).get(31)).into(awayImage);
-
-
-
-//                homeName.setText(result.get("HOME"));
-//                awayName.setText(result.get("AWAY"));
-//                homePoints.setText(result.get("HOME POINTS"));
-//                awayPoints.setText(result.get("AWAY POINTS"));
-//                homeAssists.setText(result.get("HOME ASSIST"));
-//                awayAssists.setText(result.get("AWAY ASSIST"));
-//                homeRebounds.setText(result.get("HOME REBOUND"));
-//                awayRebounds.setText(result.get("AWAY REBOUND"));
-//                homeBlocks.setText(result.get("HOME BLOCK"));
-//                awayBlocks.setText(result.get("AWAY BLOCK"));
-//                homeSteals.setText(result.get("HOME STEAL"));
-//                awaySteals.setText(result.get("AWAY STEAL"));
-//                homeTurnovers.setText(result.get("HOME TURNOVER"));
-//                awayTurnovers.setText(result.get("AWAY TURNOVER"));
-//                homeFouls.setText(result.get("HOME FOUL"));
-//                awayFouls.setText(result.get("AWAY FOUL"));
-//                homeFT.setText(result.get("HOME FTM")+"/"+result.get("HOME FTA"));
-//                awayFT.setText(result.get("AWAY FTM")+"/"+result.get("AWAY FTA"));
-//                home2pt.setText(result.get("HOME 2PM")+"/"+result.get("HOME 2PA"));
-//                away2pt.setText(result.get("AWAY 2PM")+"/"+result.get("AWAY 2PA"));
-//                home3pt.setText(result.get("HOME 3PM")+"/"+result.get("HOME 3PA"));
-//                away3pt.setText(result.get("AWAY 3PM")+"/"+result.get("AWAY 3PA"));
-
-
+                homeName.setText(result.get(2).get(0));
+                homePoints.setText(result.get(2).get(1));
+                awayName.setText(result.get(2).get(2));
+                awayPoints.setText(result.get(2).get(3));
+                Picasso.with(getApplicationContext()).load(result.get(3).get(0)).into(homeImage);
+                Picasso.with(getApplicationContext()).load(result.get(3).get(1)).into(awayImage);
             }
         }
     }
